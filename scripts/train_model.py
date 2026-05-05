fbd6da02f62325bec59c3670a34516f29ce92550
Noah Yi <noahyi7@gmail.com>
2026-05-05 16:39:29 -0500

    Update train_model to assume user starting from scratch

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -1,23 +1,23 @@
 from ultralytics import YOLO
 
 """UNCOMMENT IF YOU WANT TO RETRAIN OR TRAIN THE MODEL FROM SCRATCH"""
-# if __name__ == '__main__':
-#     # load a pretrained model
-#     model = YOLO('yolo26n.pt') # latest YOLOG pretrained pytorch model
+if __name__ == '__main__':
+    # load a pretrained model
+    model = YOLO('yolo26n.pt') # latest YOLOG pretrained pytorch model
 
-#     # train the model
-#     results = model.train(
-#         data='../data/Multi-Crop Disease Dataset/Multicrop Disease Dataset/Multicrop Disease Dataset/data.yaml', 
-#         epochs=100,
-#         imgsz=640,
-#         device='cuda', # ensure you have CUDA set up, otherwise use 'cpu'
-#         batch=8
-#     )
+    # train the model
+    results = model.train(
+        data='../data/Multi-Crop Disease Dataset/Multicrop Disease Dataset/Multicrop Disease Dataset/data.yaml', 
+        epochs=100,         # lower to <10 if want to run quick locally
+        imgsz=640,
+        device='cuda',      # ensure you have CUDA set up, otherwise use 'cpu'
+        batch=8             # lower to 4 if not enough GPU memory
+    )
 
-#     # export model
-#     model.export(
-#         format='engine'
-#     )
+    # export model
+    model.export(
+        format='engine'
+    )
 
 """UNCOMMENT IF YOU RUN INTO AN ERROR IN THE LAST MAIN"""
 # if __name__ == '__main__':
@@ -29,7 +29,8 @@ from ultralytics import YOLO
 #         format='engine'
 #     )
 
+"""UNCOMMENT IF ONLY WANT TO EXPORT"""
 # Only used after the model completed training
-if __name__ == 'main':
-    model = YOLO("runs/detect/train-6/weights/best.pt")
-    model.export(format='engine')
+# if __name__ == 'main':
+#     model = YOLO("runs/detect/train-6/weights/best.pt")
+#     model.export(format='engine')
